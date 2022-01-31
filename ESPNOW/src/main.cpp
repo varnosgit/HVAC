df51cd7cc2f45283d4260344effaf6955945717f
varnosgit <rezakarkon@gmail.com>
2022-01-31 14:03:26 +0330

    sleep + eeprom

--- ESPNOW/src/main.cpp
+++ ESPNOW/src/main.cpp
@@ -14,6 +14,10 @@
 #include "timerSetups.h"
 #include "algos.h"
 
+#define uS_TO_S_FACTOR 1000000  /* Conversion factor for micro seconds to seconds */
+#define TIME_TO_SLEEP  5        /* Time ESP32 will go to sleep (in seconds) */
+
+RTC_DATA_ATTR int bootCount = 0;
 
 bool loop_start_flag = false;
 TaskHandle_t CoreZEROTasks;
@@ -35,7 +39,7 @@ int dataCounter = 0;
 
 /////////////////////////////////////////////////////////////////////////////// esp now
 // REPLACE WITH YOUR RECEIVER MAC Address
-uint8_t WROOM_Address[] = {0x94, 0xB9, 0x7E , 0xD9 , 0xA1 ,0x04}; 
+uint8_t WROOM_Address[] = {0xFF, 0xFF, 0xFF , 0xFF , 0xFF ,0xFF}; // {0x94, 0xB9, 0x7E , 0xD9 , 0xA1 ,0x04}; 
 uint8_t TTGOPin_Address[] = {0x08, 0x3A, 0xF2 , 0x45 , 0xA5 ,0xC8};
 uint8_t TTGO_NO_Pin_Address[] = {0x84, 0xCC, 0xA8 , 0x61 , 0x4B ,0x0C};
 
@@ -61,6 +65,8 @@ void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
 // callback function that will be executed when data is received
 void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
   memcpy(&myData, incomingData, sizeof(myData));
+  Serial.printf("Message from %02X:%02X:%02X:%02X:%02X:%02X\n", mac[0], mac[1], mac[2], mac[3],
+                mac[4], mac[5]);
   Serial.print("Bytes received: ");
   Serial.println(len);
   Serial.print("Char: ");
@@ -107,7 +113,12 @@ void setup()
   // tft.setCursor(0, 0);
   // tft.setTextColor(TFT_WHITE, TFT_BLACK);
   // tft.print("No Finger    ");
+    ++bootCount;
+  Serial.println("Boot number: " + String(bootCount));
+  //logtxt1.drawNumber(getCpuFrequencyMhz(), 71, 240, 2);
+  delay(300); display_log_print("Boot number: " + String(bootCount));
 
+  //WiFi.persistent( false );
   WiFi.mode(WIFI_MODE_STA);
   Serial.println(WiFi.macAddress()); display_log_print("Wi-Fi MAC Address:"); display_log_print(WiFi.macAddress());
 
@@ -138,6 +149,11 @@ void setup()
     return;
   }
 
+     esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
+  Serial.println("Setup ESP32 to sleep for every " + String(TIME_TO_SLEEP) +
+  " Seconds");
+
+ 
 }
 
 void coreZEROTasks_code( void * pvParameters ){
@@ -171,6 +187,8 @@ void loop()
     Serial.println("Error sending the data");
   }
   delay(2000);
+
+  esp_deep_sleep_start();
 //tft.pushImage(0,0, 240, 135, info);  
 // irValue = PPG_MAX_Sensor.getIR();
 //Serial.println(irValue); //Send raw data to plotter
